8f720c0bdb9f826f227f0d4819fa0baa1c8e9392
Asher Indursky <asheri@proftit.com>
2022-01-19 11:18:44 +0200

    create an empty production angular-nest preset workspace using nx cli

--- apps/dashboard/src/app/app.module.ts
+++ apps/dashboard/src/app/app.module.ts
@@ -1,13 +1,37 @@
+import { CoreDataModule } from '@ai/core-data';
+import { CoreStateModule } from '@ai/core-state';
+import { MaterialModule } from '@ai/material';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
-import { HttpClientModule } from '@angular/common/http';
+import { RoutingModule } from './routing.module';
+import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    WidgetsComponent,
+    WidgetsListComponent,
+    WidgetDetailsComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    CoreDataModule,
+    CoreStateModule,
+    HttpClientModule,
+    MaterialModule,
+    RoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
